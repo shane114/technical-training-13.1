170350c17b629cba6b52cc2e9d1a82ca7ba8c69f
shane114 <72244493+shane114@users.noreply.github.com>
2020-10-08 16:51:25 +0800

    Initial
    with search, notebooks, edited "Description" to "Full Description"

---
+++ openacademy/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class openacademy(models.Model):
+     _name = 'openacademy.course'
+     _description = "OpenAcademy Courses"
+
+     name = fields.Char(string="Title", required=True)
+     description = fields.Text(string="Full Description")
+     about = fields.Text()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
